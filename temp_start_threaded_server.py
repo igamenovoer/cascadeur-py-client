78ef23df9b99aafd7fe3c27e6ca022c6a70dfb54
igamenovoer <igamenovoer@xx.com>
2025-08-25 20:35:36 +0800

    updated

--- temp_start_threaded_server.py
+++
@@ -1,8 +0,0 @@
-from cascadeur_client.server import health_exec_server_threaded as h
-import time
-h.start_server(port=31015)
-print('[temp] server started on 31015')
-# Keep alive briefly for test
-for i in range(5):
-    time.sleep(1)
-print('[temp] exiting')
